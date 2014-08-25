533572b6226602d99a8ce92047bcfa43a3a741df
modmuss50 <m123world@gmail.com>
2014-08-25 13:58:40 +0100

    derpy git

--- src/main/java/robomuss/rc/tracks/TrackTypeCurve.java
+++ src/main/java/robomuss/rc/tracks/TrackTypeCurve.java
@@ -2,12 +2,14 @@ package robomuss.rc.tracks;
 
 import java.util.Arrays;
 
+import net.minecraft.world.World;
 import org.lwjgl.opengl.GL11;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.entity.Entity;
 import robomuss.rc.block.render.TileEntityRenderTrack;
 import robomuss.rc.block.te.TileEntityTrack;
+import robomuss.rc.entity.EntityTrain;
 
 
 public class TrackTypeCurve extends TrackType {
@@ -64,4 +66,34 @@ public class TrackTypeCurve extends TrackType {
 			return super.getSpecialX(renderStage, z, te);
 		}
 	}*/
+
+
+    @Override
+    public void onTrainCollidedWithTrack(World world, int x, int y, int z, EntityTrain train) {
+        super.onTrainCollidedWithTrack(world, x, y, z, train);
+        if(world.isBlockIndirectlyGettingPowered(x, y -1 , z))
+            return;
+        double m_speed = 0.2D;
+        TileEntityTrack tile = (TileEntityTrack) world.getTileEntity(x, y, z);
+        int a = tile.direction;
+        int[] ax = { 0, 1, 0, -1 };
+        int[] az = { -1, 0, 1, 0 };
+
+        if (train != null) {
+
+            if ((ax[a] == 0) && (Math.abs(x + 0.5D - train.posX) < 0.5D) && (Math.abs(x + 0.5D - train.posX) > 0.1D)) {
+                train.motionX += Math.signum(x + 0.5D - train.posX) * Math.min(m_speed, Math.abs(x + 0.5D - train.posX)) / 1.2D;
+            }
+
+            if ((az[a] == 0) && (Math.abs(z + 0.5D - train.posZ) < 0.5D) && (Math.abs(z + 0.5D - train.posZ) > 0.1D)) {
+                train.motionZ += Math.signum(z + 0.5D - train.posZ) * Math.min(m_speed, Math.abs(z + 0.5D - train.posZ)) / 1.2D;
+            }
+
+            train.motionX += ax[a] * m_speed;
+
+            train.motionZ += az[a] * m_speed;
+            train.posY = y + 0.41;
+        }
+    }
+
 }
