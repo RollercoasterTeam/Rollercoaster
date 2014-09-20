cec3a577764fc1d722dacce215927c884f7283d3
modmuss50 <m123world@gmail.com>
2014-09-20 17:21:32 +0100

    fixed trains

--- src/main/java/robomuss/rc/entity/RCEntitys.java
+++ src/main/java/robomuss/rc/entity/RCEntitys.java
@@ -11,6 +11,6 @@ public class RCEntitys {
     public static void init() {
         int entityID = EntityRegistry.findGlobalUniqueEntityId();
         EntityRegistry.registerGlobalEntityID(EntityTrainDefault.class, "EntityTrain", entityID);
-        EntityRegistry.registerModEntity(EntityTrainDefault.class, "EntityTrain", entityID, RCMod.instance, 64, 5, true);
+        EntityRegistry.registerModEntity(EntityTrainDefault.class, "EntityTrain", entityID, RCMod.instance, 64, 1, true);
     }
 }
