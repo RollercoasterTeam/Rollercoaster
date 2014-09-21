1fd5e16b8870220b5756783db1e11194857fe2b1
roboyobo <km.github@gmail.com>
2014-09-21 13:47:40 +0100

    Put in cast check

--- src/main/java/robomuss/rc/block/BlockTrack.java
+++ src/main/java/robomuss/rc/block/BlockTrack.java
@@ -97,8 +97,10 @@ public class BlockTrack extends BlockContainer implements IPaintable {
     
     @Override
     public void onBlockAdded(World world, int x, int y, int z) {
-    	TileEntityTrack te = (TileEntityTrack) world.getTileEntity(x, y, z);
-    	te.type = TrackHandler.types.get(0);
+    	if(world.getTileEntity(x, y, z) != null && world.getTileEntity(x, y, z) instanceof TileEntityTrack) {
+	    	TileEntityTrack te = (TileEntityTrack) world.getTileEntity(x, y, z);
+	    	te.type = TrackHandler.types.get(0);
+    	}
     }
     
     @Override
