2a846ca639999956dc9c4d57d7bc0fea6393c9c3
modmuss50 <m123world@gmail.com>
2014-10-07 21:02:36 +0100

    derpy git

---
+++ src/main/java/robomuss/rc/item/ItemPaint.java
@@ -0,0 +1,49 @@
+package robomuss.rc.item;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import robomuss.rc.util.ColourUtil;
+
+import java.util.List;
+public class ItemPaint extends Item {
+	
+	public ItemPaint() {
+		setHasSubtypes(true);
+	}
+	
+	@Override
+	public String getUnlocalizedName(ItemStack stack) {
+		return getUnlocalizedName() + "_" + stack.getItemDamage();
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void getSubItems(Item item, CreativeTabs tab, List list) {
+		for(int i = 0; i < ColourUtil.numColours; i++) {
+			list.add(new ItemStack(item, 1, i));
+		}
+	}
+
+    //TODO come back and fix
+//	private IIcon[] icons;
+//
+//	@Override
+//	@SideOnly(Side.CLIENT)
+//	public void registerIcons(IIconRegister register) {
+//		icons = new IIcon[ColourUtil.numColours];
+//
+//		for(int i = 0; i < ColourUtil.numColours; i++) {
+//			icons[i] = register.registerIcon("rc:paint_" + i);
+//		}
+//	}
+//
+//	@Override
+//	@SideOnly(Side.CLIENT)
+//	public IIcon getIconFromDamage(int dmg) {
+//		return icons[dmg];
+//	}
+}
