b4491e1901437e5c36dc15e986a6eab7e3ac7620
roboyobo <km.github@gmail.com>
2014-12-21 14:46:57 +0000

    Added some new items

--- src/main/java/robomuss/rc/item/RCItems.java
+++ src/main/java/robomuss/rc/item/RCItems.java
@@ -1,17 +1,23 @@
 package robomuss.rc.item;
 
+import java.util.ArrayList;
+
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemArmor.ArmorMaterial;
+import net.minecraft.item.ItemFood;
 import robomuss.rc.RCMod;
 import robomuss.rc.track.TrackHandler;
 import robomuss.rc.track.extra.TrackExtra;
+import robomuss.rc.util.FoodType;
+import scala.Array;
 
 public class RCItems {
 	public static int last_extra_id;
 	
-	public static Item hammer, paint, empty_brush, brush, ticket, pass, key, train, balloon;
+	public static Item hammer, paint, empty_brush, brush, ticket, pass, key, train, balloon, coin;
+	public static ArrayList<FoodType> food = new ArrayList<FoodType>();
 	public static ItemArmor hat_1;
 	
 	public static void init() {
@@ -36,7 +42,13 @@ public class RCItems {
 		train = new ItemTrain().setUnlocalizedName("train").setTextureName("rc:train").setCreativeTab(RCMod.track);
 		hat_1 = (ItemArmor) new ItemArmor(ArmorMaterial.CHAIN, 0, 0).setUnlocalizedName("hat_1").setTextureName("rc:hat_1").setCreativeTab(RCMod.other).setMaxStackSize(1);
 		balloon = new ItemBalloon().setUnlocalizedName("balloon").setTextureName("rc:balloon").setCreativeTab(RCMod.other);
-
+		coin = new Item().setUnlocalizedName("coin").setTextureName("rc:coin").setCreativeTab(RCMod.other);
+		
+		food.add(new FoodType("Chicken Nuggets", "nuggets")); 
+		food.add(new FoodType("Chips", "chips")); 
+		food.add(new FoodType("Popcorn", "popcorn")); 
+		food.add(new FoodType("Candy Floss", "candy_floss")); 
+		
 		GameRegistry.registerItem(hammer, "hammer");
 		GameRegistry.registerItem(paint, "paint");
 		GameRegistry.registerItem(empty_brush, "empty_brush");
@@ -47,6 +59,7 @@ public class RCItems {
 		GameRegistry.registerItem(train, "train");
 		//GameRegistry.registerItem(hat_1, "hat_1");
 		GameRegistry.registerItem(balloon, "balloon");
+		GameRegistry.registerItem(coin, "coin");
 	}
 
 }
